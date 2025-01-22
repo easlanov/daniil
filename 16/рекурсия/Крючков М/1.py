b4626429e43e7b818d0cfed4b9b2e04c210497cd
Eugene Aslanov <e.aslanov@hh.ru>
2025-01-22 13:41:31 +0300

    16

--- 16/рекурсия/Крючков М/1.py
+++ 16/рекурсия/Крючков М/1.py
@@ -3,9 +3,11 @@ https://education.yandex.ru/ege/task/d6acebec-814c-4706-9c36-975c410a4a28
 
 """
 import sys
+from functools import lru_cache
 
 sys.setrecursionlimit(10000)
 
+@lru_cache(None)
 def F(n):
     if n <= 10:
         return n * 2
@@ -14,4 +16,4 @@ def F(n):
     if n % 2 != 0 and n > 10:
         return F(n - 2) * 2 - F(n - 7)
 
-print(len(F(3063)))
+print(sum(map(int, str(F(3063)))))
